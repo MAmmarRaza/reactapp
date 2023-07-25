75c30460bcc588caa1e521b88d325fd19a456e77
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2023-07-26 00:29:37 +0500

    commit again 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          Learn React Now Ammar Raza
         </a>
       </header>
     </div>
